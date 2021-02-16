ce2167ace7fd1431d7bc7824f58c304416a3e971
grzegorz.hul <grzegorz.hul@librus.pl>
2021-02-16 17:46:21 +0100

    first usable version

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,50 @@
 <template>
   <div id="app">
-    <div v-if="viewType === 'options'">
-      <p>Usatawienia łańcucha:</p>
-      <label>
-        Liczba słów:
-        <input type="text" v-model="numberOfWords">
+    <div v-if="viewType === 'main'" style="margin-bottom: 60px">
+      <h2>Generuj łańcuch</h2>
+      <label style="display: block">
+        Nazwa:<br>
+        <input type="text" v-model="chainName" class="text-field">
       </label>
-      <label>
-        Nazwa łańcucha:
-        <input type="text" v-model="chainName">
+      <label style="display: block">
+        Liczba słów (1-400):
+        <input type="text" v-model="numberOfWords" class="text-field number">
       </label>
       <button @click="generate">Generuj</button>
-    </div>
 
-    <div v-if="viewType === 'results'">
-      <div v-for="(word, i) in generatedWords" :key="i">
-        {{ word.pl.toLowerCase() }}
-        <img :src="word.img"/>
+      <h2 style="margin-top: 50px">Zapisane łańcuchy</h2>
+      <div v-for="(chain, i) in myChains" :key="i" class="saved-chains-item">
+        <span @click="() => showChain(chain.name)" style="margin-right: 20px;">{{
+            chain.name
+          }} ({{ chain.chain.length }})</span>
+        <span @click="() => removeChain(chain.name)">[usuń]</span>
       </div>
-
     </div>
+
+    <show-chain v-else-if="viewType === 'chain'"
+                @back="chandleBackButton"
+                :name="chainName"
+                :chain="generatedChain"/>
   </div>
 </template>
 
 <script>
 import {words} from './helpers/words'
+import ShowChain from "@/components/ShowChain";
 
 export default {
   name: 'App',
-  components: {},
+  components: {
+    ShowChain
+  },
   data() {
     return {
-      numberOfWords: 10,
-      chainName: '',
-      viewType: 'options', // options | results
+      numberOfWords: 4,
+      chainName: 'Łańcuch ',
+      viewType: 'main', // main | chain
       words: [],
-      generatedWords: [],
+      generatedChain: [],
+      myChains: [],
     }
   },
   created() {
@@ -55,36 +64,69 @@ export default {
       ...words.occupations,
       ...words.animals,
     ];
+    this.myChains = JSON.parse(localStorage.getItem('myChains'))
   },
   computed: {},
   methods: {
     generate() {
-
-
-      for (let i = 0; i < Number(this.numberOfWords); i++) {
-        const randomIndex = Math.floor(Math.random() * (this.words.length));
-        // dodac tak, żeby się nie powtarzało
-        const word = this.words[randomIndex]
-
-        const query =
-            `key=20280075-0a41ab4fcc29805bbe5b60f99` +
-            `&q=${word.en} isolated` +
-            `&image_type=vector` + // "all", "photo", "illustration", "vector"
-            `&safesearch=true` +
-            `&per_page=3` + // 3 - 200
-            `&image_type=illustration`
-
-        console.log(query)
-        const req = `https://pixabay.com/api/?${query}` // https://pixabay.com/api/docs/
-        fetch(req)
-            .then(resp => resp.json())
-            .then((jsonResp) => {
-              const imgUrl = jsonResp.hits[0] && jsonResp.hits[0].webformatURL ? jsonResp.hits[0].webformatURL : null
-              if (imgUrl) word.img = imgUrl
-              this.generatedWords.push(word)
-            })
+      if (this.chainName === '' || this.numberOfWords < 1 || this.numberOfWords > 400) {
+        alert('Nazwa nie może być pusta, liczba słów musi mieścić się wprzedziale od 1 do 400');
+        return;
       }
-      this.viewType = 'results'
+      const storedChains = JSON.parse(localStorage.getItem('myChains'));
+      if (storedChains) {
+        let isNameUnique = true;
+        storedChains.forEach((chain) => {
+          if (chain.name === this.chainName) isNameUnique = false;
+        })
+        if (!isNameUnique) {
+          alert('Taka nazwa już istnieje');
+          return;
+        }
+      }
+      const getUniqueRandomIndex = (times) => {
+        if (times > 0) {
+          const randomIndex = Math.floor(Math.random() * (this.words.length));
+          const word = this.words[randomIndex];
+          if (this.generatedChain.indexOf(word) === -1) {
+            this.generatedChain.push(word);
+            getUniqueRandomIndex(--times);
+          } else getUniqueRandomIndex(times);
+        }
+      }
+      getUniqueRandomIndex(this.numberOfWords);
+      const chainObj = {
+        name: this.chainName,
+        chain: this.generatedChain,
+      }
+      if (storedChains) {
+        storedChains.unshift(chainObj)
+        localStorage.setItem('myChains', JSON.stringify(storedChains))
+      } else {
+        localStorage.setItem('myChains', JSON.stringify([chainObj]))
+      }
+      this.myChains = storedChains;
+      this.viewType = 'chain';
+    },
+    showChain(chainName) {
+      const chosenChain = this.myChains.find((chain) => {
+        return chain.name === chainName;
+      })
+      this.generatedChain = chosenChain.chain;
+      this.chainName = chosenChain.name
+      this.viewType = 'chain'
+    },
+    removeChain(chainName) {
+      console.log(chainName)
+      const updatedMyChains = this.myChains.filter((chain) => {
+        return chain.name !== chainName
+      })
+      this.myChains = updatedMyChains
+      localStorage.setItem('myChains', JSON.stringify(updatedMyChains))
+    },
+    chandleBackButton() {
+      this.viewType = 'main';
+      this.generatedChain = [];
     },
   },
 }
@@ -93,10 +135,47 @@ export default {
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+label {
+  display: block;
+  margin: 20px;
+}
+
+.text-field {
+  border: 0;
+  border-bottom: solid 1px #d2d2d2;
+  width: 100%;
+  text-align: center;
+  height: 30px;
+  font: inherit;
+}
+
+.text-field:focus {
+  outline: 0;
+  border-color: black;
+}
+
+.text-field.number {
+  width: 50px;
+}
+
+button {
+  padding: 10px 50px;
+  background: antiquewhite;
+  border: solid 1px #c3b39e;
+  border-radius: 5px;
+  font: inherit;
+}
+
+hr {
+  border: 1px solid #d2d2d2;
+  margin: 30px 0;
+}
+
+.saved-chains-item {
+  padding: 10px;
 }
 </style>
---
+++ src/components/ShowChain.vue
@@ -0,0 +1,103 @@
+<template>
+  <div style="margin-bottom: 100px">
+
+    <div @click="$emit('back')" class="back-btn">&lt; wróć</div>
+    <h2>{{ name }}</h2>
+
+    <div v-if="viewType === 'single'" class="container chain-element-container">
+      <div class="chain-element">
+        {{ currentElement.toUpperCase() }}
+      </div>
+      <div class="next-button" @click="nextElement">
+        >
+      </div>
+    </div>
+
+    <div v-else class="words-container">
+      <div v-for="(word, i) in chain" :key="i" class="list-element">
+        <label>
+          {{ i + 1 }}.
+          <input type="checkbox" class="toggle" hidden/>
+          <span class="word">
+            {{ word.toUpperCase() }}
+          </span>
+        </label>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "show-chain",
+  props: {
+    chain: Array,
+    name: String,
+  },
+  data() {
+    return {
+      viewType: 'single',
+      currentElement: '',
+    }
+  },
+  mounted() {
+    this.currentElement = this.chain[0];
+  },
+  methods: {
+    nextElement() {
+      const currentElIndex = this.chain.indexOf(this.currentElement)
+      if (currentElIndex <= this.chain.length - 2) {
+        this.currentElement = this.chain[currentElIndex + 1]
+      } else {
+        this.currentElement = this.chain[0]
+        this.viewType = 'list';
+      }
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+}
+
+.chain-element-container {
+  height: calc(100vh - 100px);
+  font-size: 50px;
+}
+
+.chain-element {
+  margin-top: 100px;
+}
+
+label {
+  margin: 0;
+  padding: 10px;
+}
+
+.toggle ~ .word {
+  opacity: 0;
+  transition: opacity .3s;
+}
+
+.toggle:checked ~ .word {
+  opacity: 1;
+}
+
+.back-btn {
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  color: #0483e0;
+  padding: 20px;
+}
+
+.words-container {
+  text-align: left;
+  margin-left: 50px;
+}
+</style>
